656cf0eea6dab4d0c6edb708d24cb7d7f1034811
ManuelE <apamanuel@gmail.com>
2024-02-17 04:51:50 -0400

    Landing page done

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,9 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import style from "../Landing/Landing.module.css";
 
 const Landing = () => {
+  const navigate = useNavigate();
+  const handlerBtn = () => {
+    navigate("/home");
+  };
+
   return (
     <div className={style.container}>
-      <h1>Soy el Landing</h1>
+      <div className={style.title}>
+        <h1>Welcome to the countries app</h1>
+      </div>
+      <div className={style.containerBtn}>
+        <button className={style.btnLanding} onClick={handlerBtn}>
+          ENTER
+        </button>
+      </div>
     </div>
   );
 };
