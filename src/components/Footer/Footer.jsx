9b4dabc4f1acee28615c9fe1a3b57b4a93a065a1
ManuelE <apamanuel@gmail.com>
2024-02-17 07:26:34 -0400

    Footer done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import style from "../Footer/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={style.container}>
+      <p>2024 © Manuel Aparicio</p>
+    </div>
+  );
+};
+
+export default Footer;
