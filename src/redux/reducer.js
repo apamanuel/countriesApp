c98a1f49990eef0bbfaaa2f68a9ba845bf1534f7
ManuelE <apamanuel@gmail.com>
2024-02-19 02:11:21 -0400

    implementing Redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,180 @@
+const initialState = {
+    countriesToShow : [
+        {
+          code: "AD",
+          name: "Andorra",
+          continent: {
+            name: "Europe",
+          },
+          currency: "EUR",
+          languages: [
+            {
+              name: "Catalan",
+            },
+          ],
+        },
+        {
+          code: "AE",
+          name: "United Arab Emirates",
+          continent: {
+            name: "Asia",
+          },
+          currency: "AED",
+          languages: [
+            {
+              name: "Arabic",
+            },
+          ],
+        },
+        {
+          code: "AF",
+          name: "Afghanistan",
+          continent: {
+            name: "Asia",
+          },
+          currency: "AFN",
+          languages: [
+            {
+              name: "Pashto",
+            },
+            {
+              name: "Uzbek",
+            },
+            {
+              name: "Turkmen",
+            },
+          ],
+        },
+        {
+          code: "AG",
+          name: "Antigua and Barbuda",
+          continent: {
+            name: "North America",
+          },
+          currency: "XCD",
+          languages: [
+            {
+              name: "English",
+            },
+          ],
+        },
+        {
+          code: "AI",
+          name: "Anguilla",
+          continent: {
+            name: "North America",
+          },
+          currency: "XCD",
+          languages: [
+            {
+              name: "English",
+            },
+          ],
+        },
+        {
+          code: "AL",
+          name: "Albania",
+          continent: {
+            name: "Europe",
+          },
+          currency: "ALL",
+          languages: [
+            {
+              name: "Albanian",
+            },
+          ],
+        },
+        {
+          code: "AM",
+          name: "Armenia",
+          continent: {
+            name: "Asia",
+          },
+          currency: "AMD",
+          languages: [
+            {
+              name: "Armenian",
+            },
+            {
+              name: "Russian",
+            },
+          ],
+        },
+        {
+          code: "AO",
+          name: "Angola",
+          continent: {
+            name: "Africa",
+          },
+          currency: "AOA",
+          languages: [
+            {
+              name: "Portuguese",
+            },
+          ],
+        },
+        {
+          code: "AQ",
+          name: "Antarctica",
+          continent: {
+            name: "Antarctica",
+          },
+          currency: null,
+          languages: [],
+        },
+        {
+          code: "AR",
+          name: "Argentina",
+          continent: {
+            name: "South America",
+          },
+          currency: "ARS",
+          languages: [
+            {
+              name: "Spanish",
+            },
+            {
+              name: "Guarani",
+            },
+          ],
+        },
+        {
+          code: "AS",
+          name: "American Samoa",
+          continent: {
+            name: "Oceania",
+          },
+          currency: "USD",
+          languages: [
+            {
+              name: "English",
+            },
+            {
+              name: "Samoan",
+            },
+          ],
+        },
+        {
+          code: "AT",
+          name: "Austria",
+          continent: {
+            name: "Europe",
+          },
+          currency: "EUR",
+          languages: [
+            {
+              name: "German",
+            },
+          ],
+        },
+      ],
+};
+
+const rootReducer = (state= initialState, action)=>{
+    switch(action.type){
+        default:
+            return {... state}
+    }
+};
+
+export default rootReducer;
